aea18a7be1d80c05192fe16183b4df09a29a2d75
Naim Ben Alaya <98695786+Benalayanaim@users.noreply.github.com>
2025-04-05 22:32:33 +0200

    Update 22.Delete Node in a Linked List.py

--- Linked_List/22.Delete Node in a Linked List.py
+++ Linked_List/22.Delete Node in a Linked List.py
@@ -20,13 +20,6 @@ def deleteNode(node):
     node.next = node.next.next  # Remove the next node from the list
 
 
-
-"Explanation from My chatGpt"
-# https://chatgpt.com/c/67e892f6-e518-800f-a46c-c5d940bac296
-
-# https://chat.deepseek.com/a/chat/s/e9709600-9ed3-4f16-84ae-d62d57addfbd
-
-
 "Other vizulization of the code solution"
 # https://leetcode.com/problems/delete-node-in-a-linked-list/solutions/5113147/detailed-explanation-extremely-simple-1-2-liner-o-1-time-and-space-complexity/?envType=problem-list-v2&envId=linked-list
 # https://leetcode.com/problems/delete-node-in-a-linked-list/solutions/354949/python3-change-value-and-change-pointer/?envType=problem-list-v2&envId=linked-list
